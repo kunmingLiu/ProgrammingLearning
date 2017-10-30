2bd3799c63a234e7f52f9e74605e579d9a8f0946
Kunming Liu <kunming0525@gmail.com>
2017-10-30 11:23:44 +0800

    commit v2

---
+++ data structurue/tree/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y, count = 0;
+        while (xor != 0) {
+            xor &= (xor - 1);
+            count++;
+        }
+        return count;
+    }
+}
+----------------------------------------------
+class Solution {
+    public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x^y);
+    }
+}

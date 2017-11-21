3190604854a390131fb168a77162eafcda3e9be6
KML <kunming0525@gmail.com>
2017-11-21 12:47:35 +0800

    Update Solution.java

--- leetcode/#283 Move Zeroes/Solution.java
+++ leetcode/#283 Move Zeroes/Solution.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int hammingDistance(int x, int y) {
-        int xor = x ^ y, count = 0;
-        while (xor != 0) {
-            xor &= (xor - 1);
-            count++;
+    public void moveZeroes(int[] nums) {
+        int length = nums.length;
+        int swapIndex = 0;
+        for(int i = 0 ; i < length ; i ++){
+            if(nums[i] != 0){
+                swap(nums, i , swapIndex);
+                swapIndex++;
+            }
         }
-        return count;
     }
-}
-----------------------------------------------
-class Solution {
-    public int hammingDistance(int x, int y) {
-        return Integer.bitCount(x^y);
+    void swap(int[] nums , int index , int swapIndex){
+        int temp  = nums[index];
+        nums[index] = nums[swapIndex];
+        nums[swapIndex] = temp;
     }
 }

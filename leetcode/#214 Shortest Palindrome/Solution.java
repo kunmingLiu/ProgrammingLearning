fe2f5286a4ba77c21ae37d8feb28b59f45c65315
KML <kunming0525@gmail.com>
2017-11-21 12:46:17 +0800

    Update Solution.java

--- leetcode/#214 Shortest Palindrome/Solution.java
+++ leetcode/#214 Shortest Palindrome/Solution.java
@@ -1,16 +1,17 @@
-class Solution {
-    public int hammingDistance(int x, int y) {
-        int xor = x ^ y, count = 0;
-        while (xor != 0) {
-            xor &= (xor - 1);
-            count++;
+public class Solution {
+    public String shortestPalindrome(String s) {
+        if(s.length()<=1) return s;
+        String new_s = s+"#"+new StringBuilder(s).reverse().toString();
+        int[] position = new int[new_s.length()];
+        
+        for(int i=1;i<position.length;i++)
+        {
+            int pre_pos = position[i-1];
+            while(pre_pos>0 && new_s.charAt(pre_pos)!=new_s.charAt(i))
+                pre_pos = position[pre_pos-1];
+            position[i] = pre_pos+((new_s.charAt(pre_pos)==new_s.charAt(i))?1:0);
         }
-        return count;
-    }
-}
-----------------------------------------------
-class Solution {
-    public int hammingDistance(int x, int y) {
-        return Integer.bitCount(x^y);
+        
+        return new StringBuilder(s.substring(position[position.length-1])).reverse().toString()+s;
     }
 }

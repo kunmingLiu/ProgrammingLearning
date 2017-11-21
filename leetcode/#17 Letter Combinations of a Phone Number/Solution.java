21741bf4ae9a9f8ad046fdeb9032a6d1d7a912cd
KML <kunming0525@gmail.com>
2017-11-21 12:40:52 +0800

    Update Solution.java

--- leetcode/#17 Letter Combinations of a Phone Number/Solution.java
+++ leetcode/#17 Letter Combinations of a Phone Number/Solution.java
@@ -1,16 +1,15 @@
-class Solution {
-    public int hammingDistance(int x, int y) {
-        int xor = x ^ y, count = 0;
-        while (xor != 0) {
-            xor &= (xor - 1);
-            count++;
-        }
-        return count;
-    }
-}
-----------------------------------------------
-class Solution {
-    public int hammingDistance(int x, int y) {
-        return Integer.bitCount(x^y);
-    }
-}
+Link : https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+    
+Input:Digit string "23"
+    
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+    
+Note:
+
+Although the above answer is in lexicographical order, your answer could be in any order you want.    
+
+---------------------------------------

f8213fffc1dc5cbd59ed9f4ab502d609176ca274
KML <kunming0525@gmail.com>
2017-11-21 12:41:53 +0800

    Update Solution.java

--- leetcode/#17 Letter Combinations of a Phone Number/Solution.java
+++ leetcode/#17 Letter Combinations of a Phone Number/Solution.java
@@ -1,15 +1,20 @@
-Link : https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+class Solution {
+    private static final String[] LETTERS = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        return (digits.length() == 0) ? new ArrayList<String>() : recursiveCombine(new ArrayList<String>(), new StringBuilder() , digits , 0);
+    }
+    private List<String> recursiveCombine(List<String> results , StringBuilder builder , String digits , int index){
+        if(index == digits.length()){
+            results.add(builder.toString());
+            return  results;
+        }
 
-Given a digit string, return all possible letter combinations that the number could represent.
+        for (char c : LETTERS[Character.digit(digits.charAt(index),10)].toCharArray()) {
+            builder.append(c);
+            recursiveCombine(results , builder, digits , index + 1);
+            builder.deleteCharAt(builder.length()-1);
 
-A mapping of digit to letters (just like on the telephone buttons) is given below.
-    
-Input:Digit string "23"
-    
-Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
-    
-Note:
-
-Although the above answer is in lexicographical order, your answer could be in any order you want.    
-
----------------------------------------
+        }
+        return  results;
+    }
+}
